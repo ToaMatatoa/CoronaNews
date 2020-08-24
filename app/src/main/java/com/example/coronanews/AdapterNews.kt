9a8404894fc4859c4b4a2dcaf72280f0af40c3e1
Matatoa <rikonikotako@gmail.com>
2020-08-24 19:55:44 +0300

    AdapterNews updated

--- app/src/main/java/com/example/coronanews/AdapterNews.kt
+++ app/src/main/java/com/example/coronanews/AdapterNews.kt
@@ -1,18 +1,38 @@
 package com.example.coronanews
 
+import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.news_list_item.view.*
+import java.text.SimpleDateFormat
 
-class AdapterNews(private val items: List<String>) : RecyclerView.Adapter<AdapterNews.ViewHolder>(){
+class AdapterNews(private val listener: (News) -> Unit) :
+    RecyclerView.Adapter<AdapterNews.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(TextView(parent.context))
+    private val items = mutableListOf<News>()
+
+    fun addNews(news: List<News>) {
+        items.addAll(news)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(
+        View.inflate(parent.context, R.layout.news_list_item, null)
+    )
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.textView.text = items[position]
+        holder.bind(items[position], listener)
     }
 
     override fun getItemCount(): Int = items.size
 
-class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val dateFormatter = "DD.MM K:mm"
+        fun bind(item: News, listener: (News) -> Unit) = with(itemView) {
+            tv_source.text = item.source
+            tv_title.text = item.title
+            tv_date.text = SimpleDateFormat(dateFormatter).format(item.publishedDate)
+            setOnClickListener { listener(item) }
+        }
+    }
 }
