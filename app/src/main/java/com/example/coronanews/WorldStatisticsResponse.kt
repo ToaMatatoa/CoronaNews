a32995008ce6f8116b97e84b4bab5ffc122666a5
Matatoa <rikonikotako@gmail.com>
2020-08-28 22:42:07 +0300

    added WorldStatisticsResponse.kt

---
+++ app/src/main/java/com/example/coronanews/WorldStatisticsResponse.kt
@@ -0,0 +1,16 @@
+package com.example.coronanews
+
+import androidx.databinding.BaseObservable
+
+object WorldStatisticsResponse {
+
+    data class Result(val data: Global)
+
+    data class Global(val data: List<Data>)
+
+    data class Data(
+        val NewConfirmed: Int, val NewDeaths: Int, val NewRecovered: Int,
+        val TotalConfirmed: Int, val TotalDeaths: Int, val TotalRecovered: Int
+    ) : BaseObservable() {
+    }
+}
