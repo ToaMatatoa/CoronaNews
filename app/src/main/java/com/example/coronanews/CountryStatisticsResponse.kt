a1017550cd67debccaf460f6d5029307fb9212df
Matatoa <rikonikotako@gmail.com>
2020-08-28 22:43:25 +0300

    added CountryStatisticsResponse.kt

---
+++ app/src/main/java/com/example/coronanews/CountryStatisticsResponse.kt
@@ -0,0 +1,17 @@
+package com.example.coronanews
+
+import androidx.databinding.BaseObservable
+import java.util.*
+
+object CountryStatisticsResponse {
+
+    data class Result(val data: Country)
+
+    data class Country(val data: List<Data>)
+
+    data class Data(
+        val Active: Int, val Confirmed: Int, val Country: String,
+        val Date: Date, val Deaths: Int, val Recovered: Int
+    ) : BaseObservable() {
+    }
+}
