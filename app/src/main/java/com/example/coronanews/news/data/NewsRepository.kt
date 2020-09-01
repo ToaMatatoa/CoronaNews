8f4dc0dbd5a781aa5b2d61c82010797920f2b2b2
Matatoa <rikonikotako@gmail.com>
2020-09-02 00:11:05 +0300

    updated NewsRepository.kt

--- app/src/main/java/com/example/coronanews/news/data/NewsRepository.kt
+++ app/src/main/java/com/example/coronanews/news/data/NewsRepository.kt
@@ -1,13 +1,13 @@
 package com.example.coronanews.news.data
 
-import com.example.coronanews.news.model.NewsDataItems
+import com.example.coronanews.news.model.NewsResponse
 import io.reactivex.Single
 
 class NewsRepository {
 
     private val remoteDS = RemoteNewsDataSource()
 
-    fun getNewsList(): Single<List<NewsDataItems>> {
+    fun getNewsList(): Single<List<NewsResponse.Article>> {
         return remoteDS.getNewsList()
     }
 }
