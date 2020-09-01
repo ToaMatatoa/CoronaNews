4954fd9dada45f44472a946ebc1b88fbf536c31a
Matatoa <rikonikotako@gmail.com>
2020-09-02 00:11:40 +0300

    added RemoteNewsDataSource.kt

---
+++ app/src/main/java/com/example/coronanews/news/data/RemoteNewsDataSource.kt
@@ -0,0 +1,16 @@
+package com.example.coronanews.news.data//package com.example.coronanews.data.newsDataItems
+
+import com.example.coronanews.news.model.NewsResponse
+import io.reactivex.Single
+
+class RemoteNewsDataSource {
+
+    private val retrofitNewsService by lazy { RetrofitClient.newsWebService() }
+
+    fun getNewsList(): Single<List<NewsResponse.Article>> {
+        return retrofitNewsService.getNewsList(
+            country = "UA", topic = "newsDataItems", language = "ru",
+            sortBy = "date", pageSize = 20, haveImage = true, pageNumber = 1, searchWord = "covid"
+        ).map { it.data }
+    }
+}
