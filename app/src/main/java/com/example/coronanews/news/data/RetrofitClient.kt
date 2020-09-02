bbf8938f19df09986f97ec17fcfa1f95a3097328
Matatoa <rikonikotako@gmail.com>
2020-09-02 12:11:20 +0300

    updated RetrofitClient.kt

--- app/src/main/java/com/example/coronanews/news/data/RetrofitClient.kt
+++ app/src/main/java/com/example/coronanews/news/data/RetrofitClient.kt
@@ -3,22 +3,27 @@ package com.example.coronanews.news.data
 import com.example.coronanews.graph.data.RetrofitStatisticsService
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
+
 object RetrofitClient {
 
-    private const val newsURL = "https://covid-19-newsDataItems.p.rapidapi.com/"
+    private const val newsURL = "https://covid-19-news.p.rapidapi.com/"
     private const val statisticsURL = "https://api.covid19api.com/"
 
     fun newsWebService(): RetrofitNewsService {
+
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BODY
 
         val okHttpClient = OkHttpClient.Builder().apply {
             addInterceptor(
                 Interceptor { chain ->
                     val builder = chain.request().newBuilder()
-                    builder.header("X-RapidAPI-Host", "covid-19-newsDataItems.p.rapidapi.com")
+                    builder.header("X-RapidAPI-Host", "covid-19-news.p.rapidapi.com")
                     builder.header(
                         "X-RapidAPI-Key",
                         "1fb34b6850mshf697836118ae0aep110610jsn7c63a5dfe31c"
@@ -26,6 +31,8 @@ object RetrofitClient {
                     return@Interceptor chain.proceed((builder.build()))
                 }
             )
+            addInterceptor(NewsResponseInterceptor())
+            addInterceptor(logging)
         }.build()
 
         val retrofit = Retrofit.Builder()
@@ -34,7 +41,6 @@ object RetrofitClient {
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-
 
         return retrofit.create(RetrofitNewsService::class.java)
     }
