19f272de2c29017b47ab47cf450e30772762cbcc
Matatoa <rikonikotako@gmail.com>
2020-09-11 12:25:37 +0300

    update RetrofitStatisticsService.kt

--- app/src/main/java/com/example/coronanews/graph/data/RetrofitStatisticsService.kt
+++ app/src/main/java/com/example/coronanews/graph/data/RetrofitStatisticsService.kt
@@ -1,7 +1,7 @@
 package com.example.coronanews.graph.data
 
-import com.example.coronanews.graph.model.CountryStatistics
-import com.example.coronanews.graph.model.WorldStatistics
+import com.example.coronanews.graph.model.CountryStatisticsResponse
+import com.example.coronanews.graph.model.WorldStatisticsResponse
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -11,7 +11,7 @@ interface RetrofitStatisticsService {
     @GET("")
     fun getWorldStatisticsList (
         @Query("summary") worldStatistics: String
-    ) : Single<List<WorldStatistics>>
+    ) : Single<WorldStatisticsResponse>
 
     @GET("")
     fun getCountryStatisticsList(
@@ -20,5 +20,5 @@ interface RetrofitStatisticsService {
         @Query("europe") continent: String,
         @Query("status") status: String,
         @Query("confirmed") confirmed: Boolean
-    ) : Single<List<CountryStatistics>>
+    ) : Single<CountryStatisticsResponse>
 }
