a426c935f09efddbea6d26835ab82184a452df10
Matatoa <rikonikotako@gmail.com>
2020-09-11 12:29:36 +0300

    updated CountryStatisticsResponse.kt

--- app/src/main/java/com/example/coronanews/graph/model/CountryStatisticsResponse.kt
+++ app/src/main/java/com/example/coronanews/graph/model/CountryStatisticsResponse.kt
@@ -1,17 +1,21 @@
 package com.example.coronanews.graph.model
 
-import androidx.databinding.BaseObservable
+import com.google.gson.annotations.SerializedName
 import java.util.*
 
-object CountryStatisticsResponse {
+class CountryStatisticsResponse(
+    @SerializedName("Countries") var data: List<Country>
+) {
 
-    data class Result(val data: Country)
+    data class Country(
+        @SerializedName("Country") val country: String,
+        @SerializedName("NewConfirmed") val newConfirmed: Int,
+        @SerializedName("TotalConfirmed") val totalConfirmed: Int,
+        @SerializedName("NewDeaths") val newDeaths: Int,
+        @SerializedName("TotalDeaths") val totalDeaths: Int,
+        @SerializedName("NewRecovered") val newRecovered: Int,
+        @SerializedName("TotalRecovered") val totalRecovered: Int,
+        @SerializedName("Date") val date: Date,
+    )
 
-    data class Country(val data: List<Data>)
-
-    data class Data(
-        val Active: Int, val Confirmed: Int, val Country: String,
-        val Date: Date, val Deaths: Int, val Recovered: Int
-    ) : BaseObservable() {
-    }
 }
