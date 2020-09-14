4822eeec41f6a7882a5b4cc4ef07925b569f8393
Matatoa <rikonikotako@gmail.com>
2020-09-15 00:22:57 +0300

    updated GraphFragment.kt (added bar chart), updated fragment_graph.xml (bar chart), updated colors.xml

--- app/src/main/java/com/example/coronanews/graph/ui/GraphFragment.kt
+++ app/src/main/java/com/example/coronanews/graph/ui/GraphFragment.kt
@@ -9,9 +9,8 @@ import com.example.coronanews.R
 import com.example.coronanews.graph.model.CountryLiveResponse
 import com.example.coronanews.graph.model.SummaryStatisticsResponse
 import com.github.mikephil.charting.components.Legend
-import com.github.mikephil.charting.data.PieData
-import com.github.mikephil.charting.data.PieDataSet
-import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.components.LegendEntry
+import com.github.mikephil.charting.data.*
 import com.github.mikephil.charting.utils.ColorTemplate
 import kotlinx.android.synthetic.main.fragment_graph.*
 
@@ -61,6 +60,69 @@ class GraphFragment : Fragment(), GraphContract.View {
 
     override fun showCountryStatistics(countryStatisticsDataItems: List<SummaryStatisticsResponse.Country>) {
 
+        val legend = bar_chart.legend
+        val legendEntryTotal = LegendEntry()
+        legendEntryTotal.label = "Total"
+        legendEntryTotal.formColor = resources.getColor(R.color.yellow)
+        val legendEntryRecovered = LegendEntry()
+        legendEntryRecovered.label = "Recovered"
+        legendEntryRecovered.formColor = resources.getColor(R.color.orange)
+        val legendEntryDied = LegendEntry()
+        legendEntryDied.label = "Died"
+        legendEntryDied.formColor = resources.getColor(R.color.red)
+        val legendEntryTotalDay = LegendEntry()
+        legendEntryTotalDay.label = "Total day"
+        legendEntryTotalDay.formColor = resources.getColor(R.color.palegreen)
+        val legendEntryRecoveredDay = LegendEntry()
+        legendEntryRecoveredDay.label = "Recovered day"
+        legendEntryRecoveredDay.formColor = resources.getColor(R.color.lightseagreen)
+        val legendEntryDiedDay = LegendEntry()
+        legendEntryDiedDay.label = "Died day"
+        legendEntryDiedDay.formColor = resources.getColor(R.color.green)
+
+        legend.form = Legend.LegendForm.CIRCLE
+        legend.verticalAlignment = Legend.LegendVerticalAlignment.BOTTOM
+        legend.horizontalAlignment = Legend.LegendHorizontalAlignment.CENTER
+        legend.orientation = Legend.LegendOrientation.HORIZONTAL
+        legend.textSize = 10.5f
+        legend.setCustom(
+            listOf(
+                legendEntryTotal,
+                legendEntryRecovered,
+                legendEntryDied,
+                legendEntryTotalDay,
+                legendEntryRecoveredDay,
+                legendEntryDiedDay
+            )
+        )
+
+        val entries: MutableList<BarEntry> = ArrayList()
+        entries.add(BarEntry(1f, countryStatisticsDataItems[176].totalConfirmed.toFloat()))
+        entries.add(BarEntry(2f, countryStatisticsDataItems[176].totalRecovered.toFloat()))
+        entries.add(BarEntry(3f, countryStatisticsDataItems[176].totalDeaths.toFloat()))
+        entries.add(BarEntry(4f, countryStatisticsDataItems[176].newConfirmed.toFloat()))
+        entries.add(BarEntry(5f, countryStatisticsDataItems[176].newRecovered.toFloat()))
+        entries.add(BarEntry(6f, countryStatisticsDataItems[176].newDeaths.toFloat()))
+        val set = BarDataSet(entries, "")
+
+        set.setColors(
+            resources.getColor(R.color.yellow),
+            resources.getColor(R.color.orange),
+            resources.getColor(R.color.red),
+            resources.getColor(R.color.palegreen),
+            resources.getColor(R.color.lightseagreen),
+            resources.getColor(R.color.green)
+        )
+
+        val data = BarData(set)
+        data.setValueTextSize(11f)
+        data.barWidth = 0.9f
+
+        bar_chart.data = data
+        bar_chart.setFitBars(true)
+        bar_chart.invalidate()
+        bar_chart.description.isEnabled = false
+        bar_chart.animateXY(1000, 1000)
     }
 
     override fun showLiveStatistics(liveStatisticsDataItem: List<CountryLiveResponse>) {
